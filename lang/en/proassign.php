5efd06a1a69b58a6d7f393db856a56b5b7a9feff
isur-c <isuru.13@cse.mrt.ac.lk>
2016-05-25 01:15:40 +0530

    Initial commit

---
+++ lang/en/proassign.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for proassign
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package    mod_proassign
+ * @copyright  2015 Your Name
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'Programming Assignment';
+$string['modulenameplural'] = 'proassigns';
+$string['modulename_help'] = 'Use the proassign module for... | The proassign module allows...';
+$string['proassignfieldset'] = 'Custom example fieldset';
+$string['proassignname'] = 'proassign name';
+$string['proassignname_help'] = 'This is the content of the help tooltip associated with the proassignname field. Markdown syntax is supported.';
+$string['proassign'] = 'proassign';
+$string['pluginadministration'] = 'proassign administration';
+$string['pluginname'] = 'Programming Assignment';
